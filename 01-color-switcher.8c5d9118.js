809683264d9d08efca7d5aa4b040472b8cbfaf9d
narovskyi <60262927+narovskyi@users.noreply.github.com>
2022-11-21 16:17:15 +0000

    Deploying to gh-pages from @ narovskyi/goit-js-hw-09@7ae6da751e8bde1d22e0d5e00a3dec94853a6ff1 🚀

--- 01-color-switcher.8c5d9118.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){o(t.startBtn),r=t.stopBtn,r.removeAttribute("disabled"),e=setInterval(n,1e3,t.body);var r})),t.stopBtn.addEventListener("click",onStopBtnClick),o(t.stopBtn);let e=null;function n(t){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}function o(t){t.setAttribute("disabled","disabled")}
-//# sourceMappingURL=01-color-switcher.8c5d9118.js.map
